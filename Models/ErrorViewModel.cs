cc0025039a8cf877a5f6903eee0bd831cd05f909
kishalaya1 <kishalaya1@yahoo.com>
2023-01-08 22:37:09 +0530

    clean up
    clear up all files

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomHelper.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
